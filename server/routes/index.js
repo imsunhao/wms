f54843766d93ee9be396e2bc6ad07811c244257a
imzhangxing <1196087352@qq.com>
2017-03-07 17:23:47 +0800

    修改了，登录返回信息的bug

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,1 +1,36 @@
-var express=require("express"),router=express.Router();router.param("_id",function(e,r,o,s){console.log("CALLED ONLY ONCE"+s),o()}),router.get("/",function(e,r){r.render("index",{title:"主页",user:e.session.user})}),module.exports=router;
+
+// TODO 首页
+
+var express = require('express');
+var router = express.Router();
+
+router.param('_id', function (req, res, next, id) {
+    console.log('CALLED ONLY ONCE'+id);
+    next();
+});
+router.param('_pageName', function (req, res, next, id) {
+    console.log('CALLED ONLY ONCE'+id);
+    next();
+});
+
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: '主页',
+        user: req.session.user
+    });
+});
+
+//  TODO 动态加载页面
+
+router.get('page/:_pageName', function (req, res, next) {
+
+    var option={};
+
+    switch (req.body.params._pageName){
+        default:
+            option={};
+    }
+    res.render('page/'+req.body.params._pageName, option);
+});
+
+module.exports = router;
--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -1,1 +1,22 @@
-var express=require("express"),router=express.Router();router.param("_id",function(e,r,o,s){console.log("CALLED ONLY ONCE"+s),o()}),router.get("/",function(e,r){r.render("index",{title:"主页",user:e.session.user})}),module.exports=router;
+
+// TODO 业务处理
+
+var express = require('express');
+var router = express.Router();
+
+
+/*Set routerParam*/
+router.param('_id', function (req, res, next, id) {
+    console.log('CALLED ONLY ONCE'+id);
+    next();
+});
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: '主页',
+        user: req.session.user
+    });
+});
+
+module.exports = router;
