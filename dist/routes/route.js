df698b0704f36e867ce6516400b440fb46fc3e87
imzhangxing <1196087352@qq.com>
2017-05-08 20:36:38 +0800

    循环盘点 post方法修改 TODOLIST参数添加 route.js路由添加

--- dist/routes/route.js
+++ dist/routes/route.js
@@ -960,36 +960,47 @@ router.param('_url', function (req, res, next, url) {
               case 'loopInventory':
                 switch (parseInt(req.params._status)) {
                     case 0:
-                        urlName = '循环盘点  分页查询';
-                        req = autoUrl(req, '', "POST", function (json) {
+                        urlName = '循环盘点  循环盘点分页查询';
+                        req = autoUrl(req, '/mfunpd/selectXHPDByPage', "POST", function (json) {
                             res.send(json);
                         });
                         break;
                     case 1:
-                        urlName = '循环盘点 新增';
-                        req = autoUrl(req, '', "POST", function (json) {
+                        urlName = '循环盘点 新增循环盘点计划及明细';
+                        req = autoUrl(req, '/mfunpd/insertXHMfunpdDocAndDocs', "POST", function (json) {
                             res.send(json);
                         });
                         break;
                     case 2:
-                        urlName = '循环盘点 分配任务';
-                        req = autoUrl(req, '', "POST", function (json) {
+                        urlName = '循环盘点 根据盘点明细id重盘（修改状态）';
+                        req = autoUrl(req, '/mfunpd/againPdByPdsId', "POST", function (json) {
                             res.send(json);
                         });
                         break;
                       case 3:
-                        urlName = '循环盘点 盘点详情';
-                        req = autoUrl(req, '', "POST", function (json) {
+                        urlName = '循环盘点 根据盘点明细id分配任务';
+                        req = autoUrl(req, '/mfunpd/allocatingTaskByPdsId', "POST", function (json) {
                             res.send(json);
                         });
                         break;
                       case 4:
-                        urlName = '循环盘点 盘点确认';
-                        req = autoUrl(req, '', "POST", function (json) {
+                        urlName = '循环盘点 根据盘点计划id确认盘点信息（修改库存）';
+                        req = autoUrl(req, '/mfunpd/confirmPdDocByPdId', "POST", function (json) {
                             res.send(json);
                         });
                         break;
-
+                        case 5:
+                        urlName = '循环盘点 分配任务页面根据盘点计划ID查询详情';
+                        req = autoUrl(req, '/mfunpd/selectPdInfoByPdId', "POST", function (json) {
+                            res.send(json);
+                        });
+                        break;
+                        case 6:
+                        urlName = '循环盘点 盘点详情页面根据盘点计划ID查询详情';
+                        req = autoUrl(req, '/mfunpd/selectPdInfoByPdsPdId', "POST", function (json) {
+                            res.send(json);
+                        });
+                        break;
                 }
                 break;
             case 'comprehensiveInventory':
