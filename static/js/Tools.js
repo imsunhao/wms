a7bb44b71a9ed7b5a5443995cb1a785810739bd6
imzhangxing <1196087352@qq.com>
2017-05-27 10:44:45 +0800

    盘点查询 时间显示秒

--- static/js/Tools.js
+++ static/js/Tools.js
@@ -194,7 +194,7 @@ function auto_time_new(value, number) {
         case 4:
             return dateFormat(new Date(value), 'yyyy/MM/dd hh:mm');
         case 5:
-            return dateFormat(new Date(value), 'yyyy年 MM月 dd日 hh:mm');
+            return dateFormat(new Date(value), 'yyyy年 MM月 dd日 hh:mm:ss');
         default:
             return dateFormat(new Date(value), 'yyyy-MM-dd');
     }
