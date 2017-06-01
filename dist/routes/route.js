c984c888977e14e849706e8033101422c5a782a6
imzhangxing <1196087352@qq.com>
2017-06-01 17:30:36 +0800

    入库台账 导出

--- dist/routes/route.js
+++ dist/routes/route.js
@@ -317,7 +317,7 @@ router.param('_url', function (req, res, next, url) {
                         break;
                     case 3:
                         urlName = '导出';
-                        req = autoUrl(req, '', "POST", function (json) {
+                        req = autoUrl(req, '/mfunrktzDoc/exportTzExcel', "POST", function (json) {
                             res.send(json);
                         });
                         break;
