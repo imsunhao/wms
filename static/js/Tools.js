bdd293dba1e3743644ac5dc565b7cd25980e6854
imsunhao <sunhao__159@163.com>
2017-07-06 10:10:54 +0800

    修正 车辆管理 车牌号验证

--- static/js/Tools.js
+++ static/js/Tools.js
@@ -447,7 +447,7 @@ function autoValidate(option, cbs) {
 
         v_Car: function (rule, value, callback) {
           var reg = new RegExp("^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$","g");
-            if (reg.test(value)) {
+            if (!reg.test(value)) {
                 return callback(new Error('必须为中国车牌号!'));
             }
             else {
