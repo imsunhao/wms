ce152e3df953a0637fea8d575729ab32e5465f3d
imsunhao <sunhao__159@163.com>
2017-11-07 10:48:01 +0800

    补丁 20171107#000001            1. 修复 货品编号 查重的问题 改为 POST

--- dist/routes/route.js
+++ dist/routes/route.js
@@ -469,7 +469,7 @@ router.param('_url', function (req, res, next, url) {
             break;
           case 9:
             urlName = '货品编号  判断货品编号是否重复';
-            req = autoUrl(req, '/goods/isRepeatByBgGoodsNo' + req.query.bgGoodsNo, "GET", function (json) {
+            req = autoUrl(req, '/goods/isRepeatByBgGoodsNo', "POST", function (json) {
               res.send(json);
             });
             break;
